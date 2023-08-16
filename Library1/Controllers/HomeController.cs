2e252573a2e787229664c82165b739f0fd63d73d
09 <09@DESKTOP-CEM26G1>
2023-08-16 18:16:20 +0500

    Add project files.

---
+++ Library1/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿
+using Library1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Library.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private static List<Book> books = new List<Book>
+        {
+            new Book
+            {
+                Id = 1,
+                Name = "Apple",
+                Author = "Stiv Jobs",
+                Price = 500000
+            },
+            new Book
+            {
+                Id = 2,
+                Name = "Dunyo Ishlari",
+                Author = "H.To'xtaboyev",
+                Price = 30000
+            },
+           
+        };
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(books);
+        }
+        public IActionResult Book()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Book book)
+        {
+            books.Add(book);
+            return RedirectToAction("index");
+        }
+    }
+}
